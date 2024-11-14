9b22f72ab30d86d2a5788d39a9e92a508c725b0b
Kalana0001 <141452020+Kalana0001@users.noreply.github.com>
2024-11-14 18:57:58 +0530

    35commit

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // Create a connection pool for the database
 const db = mysql.createPool({
     host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     waitForConnections: true,
